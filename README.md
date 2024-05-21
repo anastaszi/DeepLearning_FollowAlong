82811f6cef2df25f5a6bc3c4b779ec5b796d9556
Anastasia Zimina <anastasba@gmail.com>
2024-05-20 19:55:44 -0700

    adding first logic

--- README.md
+++ README.md
@@ -11,7 +11,20 @@ pinned: false
 
 Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
 To run the app localy, follow the instrucctions below.
-1. Install python
+1. Install python 12.0 or higher
+
+2. Set up OpenAI API key
+```
+echo "export OPENAI_API_KEY='yourkey'" >> ~/.zshrc
+```
+2. Update the shell with the new variable:
+```
+source ~/.zshrc
+```
+2. Confirm that you have set your environment variable using the following command. 
+```
+echo $OPENAI_API_KEY
+```
 2. Create a virtual environment
 ```
 python3 -m venv streamlit-env
@@ -32,4 +45,4 @@ streamlit run app.py
 7. To deactivate the virtual environment
 ```
 deactivate
-```python
+```
--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import warnings
+import crewai
+import os
+import langchain_openai
+from langchain_openai import OpenAI
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+from crewai import Agent, Task, Crew
+import utils
+warnings.filterwarnings("ignore")
+
+if not os.environ.get("OPENAI_API_KEY"):
+    st.sidebar.warning('Please add your OpenAI API key to environment variables.')
+    OPENAI_API_KEY=st.sidebar.text_input('Enter your OpenAI API key', type='password')
+else:
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+
+models = utils.load_json('data/models.json')["models"]
+name_to_model = {model["name"]: model["model"] for model in models}
+selected_model_name = st.sidebar.selectbox('Select a model', list(name_to_model.keys()))
+selected_model = name_to_model[selected_model_name]
+
+llm=OpenAI(api_key=OPENAI_API_KEY, model_name=selected_model)
+
+
+#st.write('Selected model:', model)
