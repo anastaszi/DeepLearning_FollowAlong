025d7f59b84e128910aea2b1f6d3d3679bef3e31
Anastasia Zimina <anastasba@gmail.com>
2024-05-21 23:32:24 -0700

    updating app.py" "

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ from utils import load_json
 
 warnings.filterwarnings("ignore")
 
-NUMBER_OF_COLUMNS = 3
-
-
+# UI utils
 def add_agent():
     st.session_state.agents.append({
+        'name': '',
         'role': '',
         'backstory': '',
         'goal': '',
@@ -22,17 +21,34 @@ def add_agent():
         'verbose': True
     })
 
+def add_tasks():
+    st.session_state.tasks.append({
+        'name': '',
+        'description': '',
+        'expected_output': '',
+        'agent': ''
+    })
+
 def remove_agent(index):
     st.session_state.agents.pop(index)
 
+def remove_task(index):
+    st.session_state.tasks.pop(index)  
+
 research_agents = load_json('data/research_example.json')["agents"]
+research_tasks = load_json('data/research_example.json')["tasks"]
 
 def fill_example_agents():
     if research_agents and isinstance(research_agents, list) and len(research_agents) > 0:
         st.session_state.agents = research_agents
 
+def fill_example_tasks():
+    if research_tasks and isinstance(research_tasks, list) and len(research_tasks) > 0:
+        st.session_state.tasks = research_tasks
+
 if 'agents' not in st.session_state:
     st.session_state.agents = []
+    st.session_state.tasks = []
 
 
 if not os.environ.get("OPENAI_API_KEY"):
@@ -53,7 +69,11 @@ if not OPENAI_API_KEY:
    st.stop()
 
 llm=OpenAI(api_key=OPENAI_API_KEY, model_name=selected_model)
-
+with st.sidebar:
+    st.divider()
+    st.subheader('Research Example')
+    st.button('Fill example research agents', on_click=fill_example_agents)
+    st.button('Fill example research tasks', on_click=fill_example_tasks)
 st.code(
 '''
     agent = Agent(
@@ -69,27 +89,59 @@ st.code(
         allow_delegation=False,
         verbose=True'''
 )
-with st.sidebar:
-    st.divider()
-    st.subheader('Research Example')
-    st.button('Fill example research agents', on_click=fill_example_agents)
 
-st.divider()
+
 
 for i, agent in enumerate(st.session_state.agents):
     st.subheader(f"Agent #{i + 1}")
     c1, c2 = st.columns([3, 1])
     with c1:
+        agent['name'] = st.text_input('Agent name', key=f"agent_name_{i}", value=agent['name'], placeholder="Agent")
         agent['role'] = st.text_input('Agent role', key=f"role_{i}", value=agent['role'], placeholder="Assistant")
         agent['goal'] = st.text_input('Agent goal', key=f"goal_{i}", value=agent['goal'], placeholder="Help users with their queries" )
         agent['backstory'] = st.text_area('Agent backstory', key=f"backstory_{i}", value=agent['backstory'], placeholder="I am a virtual assistant")
     with c2:
-        st.caption('Options')
+        st.caption('Optional attributes')
         agent['allow_delegation'] = st.checkbox('Allow delegation', key=f"delegation_{i}", value=agent['allow_delegation'])
         agent['verbose'] = st.checkbox('Verbose', key=f"verbose_{i}", value=agent['verbose'])
         st.button('➖ Remove agent', on_click=remove_agent, args=(i,), key=f"remove_{i}")
 
 st.button('➕ Add agent', on_click=add_agent)
+
+st.divider()
+st.code(
+'''
+    task = Task(
+    description=(
+        "1. Prioritize the latest trends, key players, "
+        "and noteworthy news on {topic}."
+        "2. Identify the target audience, considering "
+        "their interests and pain points."
+        "3. Develop a detailed content outline including "
+        "an introduction, key points, and a call to action."
+        "4. Include SEO keywords and relevant data or sources."
+    ),
+    expected_output="A comprehensive content plan document "
+        "with an outline, audience analysis, "
+        "SEO keywords, and resources.",
+    agent=planner,
+)
+'''
+)
+
+
+for i, task in enumerate(st.session_state.tasks):
+    st.subheader(f"Task #{i + 1}")
+    c1, c2 = st.columns([3, 1])
+    with c1:
+        task['name'] = st.text_input('Agent name', key=f"task_name_{i}", value=task['name'], placeholder="TaskName")
+        task['description'] = st.text_area('Task description', key=f"task_description_{i}", value=task['description'], placeholder="Task description")
+        task['expected_output'] = st.text_area('Expected output', key=f"expected_output_{i}", value=task['expected_output'], placeholder="Expected output")
+    with c2:
+        task['agent'] = st.selectbox('Select an agent', [agent['name'] for agent in st.session_state.agents], key=f"task_agent_{i}")
+        st.button('➖ Remove task', on_click=remove_task, args=(i,), key=f"remove_task_{i}")
+
+st.button('➕ Add task', on_click=add_tasks)
 
 # Debug information
 st.write(st.session_state.agents)
